1d50e45bca42d946d9c19130ae9cb5726fb6814a
Carlos Zamora <czamora5@ucol.mx>
2022-10-31 15:38:32 -0600

    feat: improve search bar filter

--- src/app/js/searchBar/search.js
+++ src/app/js/searchBar/search.js
@@ -1,5 +1,14 @@
-function isSubstring(str1, str2) {
-  return str1.toLowerCase().includes(str2.toLowerCase());
+function isSubstring(word, pattern) {
+  let ptr = 0;
+  word = word.toLowerCase();
+  pattern = pattern.toLowerCase();
+
+  for (let i = 0; i < word.length; i++) {
+    if (ptr === pattern.length) break;
+    if (word[i] === pattern[ptr]) ptr++;
+  }
+
+  return ptr === pattern.length;
 }
 
 export function search(searchTerm, list, predicate = isSubstring) {
--- src/app/js/searchBar/searchBar.js
+++ src/app/js/searchBar/searchBar.js
@@ -3,9 +3,9 @@ import { search } from './search.js';
 import { printSearchResults } from './printSearchResults.js';
 import { clearElementContent } from '../utils/clearElementContent.js';
 
-function handleChange(input, resultsList, breedNames) {
-  const searchTerm = input.value.trim();
+function handleChange(searchTerm, resultsList, breedNames) {
   const searchResults = search(searchTerm, breedNames);
+  clearElementContent(resultsList);
   printSearchResults(resultsList, searchResults);
 }
 
@@ -13,10 +13,14 @@ export function searchBar(searchInputSelector, resultsSelector, breeds) {
   const input = document.querySelector(searchInputSelector);
   const resultsList = document.querySelector(resultsSelector);
   const breedNames = breeds.map(elem => capitalize(elem.name));
+  let searchTerm = '';
 
-  handleChange(input, resultsList, breedNames);
-  input.addEventListener('keydown', () => {
-    clearElementContent(resultsList);
-    handleChange(input, resultsList, breedNames);
+  handleChange(searchTerm, resultsList, breedNames);
+  input.addEventListener('input', evt => {
+    const newSearchTerm = evt.target.value.trim();
+    if (searchTerm === newSearchTerm) return;
+
+    searchTerm = newSearchTerm;
+    handleChange(searchTerm, resultsList, breedNames);
   });
 }
